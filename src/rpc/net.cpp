81de632761fe2d0072a1f1ce1aa9d15fab2d612f
Wladimir J. van der Laan <laanwj@gmail.com>
2016-11-21 13:55:35 +0100

    rpc: Named arguments for net calls

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -431,11 +431,11 @@ UniValue setban(const JSONRPCRequest& request)
     if (request.fHelp || request.params.size() < 2 ||
         (strCommand != "add" && strCommand != "remove"))
         throw std::runtime_error(
-            "setban \"ip(/netmask)\" \"add|remove\" ( bantime absolute )\n"
+            "setban \"subnet\" \"add|remove\" ( bantime absolute )\n"
             "\nAttempts add or remove a IP/Subnet from the banned list.\n"
 
             "\nArguments:\n"
-            "1. \"ip(/netmask)\" (string, required) The IP/Subnet (see getpeerinfo for nodes ip) with a optional netmask (default is /32 = single ip)\n"
+            "1. \"subnet\"       (string, required) The IP/Subnet (see getpeerinfo for nodes ip) with a optional netmask (default is /32 = single ip)\n"
             "2. \"command\"      (string, required) 'add' to add a IP/Subnet to the list, 'remove' to remove a IP/Subnet from the list\n"
             "3. \"bantime\"      (numeric, optional) time in seconds how long (or until when if [absolute] is set) the ip is banned (0 or empty means using the default time of 24h which can also be overwritten by the -bantime startup argument)\n"
             "4. \"absolute\"     (boolean, optional) If set, the bantime must be a absolute timestamp in seconds since epoch (Jan 1 1970 GMT)\n"
@@ -552,19 +552,19 @@ UniValue clearbanned(const JSONRPCRequest& request)
 }
 
 static const CRPCCommand commands[] =
-{ //  category              name                      actor (function)         okSafeMode
-  //  --------------------- ------------------------  -----------------------  ----------
-    { "network",            "getnetworkinfo",         &getnetworkinfo,         true  },
-    { "network",            "addnode",                &addnode,                true  },
-    { "network",            "disconnectnode",         &disconnectnode,         true  },
-    { "network",            "getaddednodeinfo",       &getaddednodeinfo,       true  },
-    { "network",            "getconnectioncount",     &getconnectioncount,     true  },
-    { "network",            "getnettotals",           &getnettotals,           true  },
-    { "network",            "getpeerinfo",            &getpeerinfo,            true  },
-    { "network",            "ping",                   &ping,                   true  },
-    { "network",            "setban",                 &setban,                 true  },
-    { "network",            "listbanned",             &listbanned,             true  },
-    { "network",            "clearbanned",            &clearbanned,            true  },
+{ //  category              name                      actor (function)         okSafe argNames
+  //  --------------------- ------------------------  -----------------------  ------ --------
+    { "network",            "getnetworkinfo",         &getnetworkinfo,         true,  {}  },
+    { "network",            "addnode",                &addnode,                true,  {"node","command"}  },
+    { "network",            "disconnectnode",         &disconnectnode,         true,  {"node"}  },
+    { "network",            "getaddednodeinfo",       &getaddednodeinfo,       true,  {"dummy","node"}  },
+    { "network",            "getconnectioncount",     &getconnectioncount,     true,  {}  },
+    { "network",            "getnettotals",           &getnettotals,           true,  {}  },
+    { "network",            "getpeerinfo",            &getpeerinfo,            true,  {}  },
+    { "network",            "ping",                   &ping,                   true,  {}  },
+    { "network",            "setban",                 &setban,                 true,  {"subnet", "command", "bantime", "absolute"}  },
+    { "network",            "listbanned",             &listbanned,             true,  {}  },
+    { "network",            "clearbanned",            &clearbanned,            true,  {}  },
 };
 
 void RegisterNetRPCCommands(CRPCTable &tableRPC)
