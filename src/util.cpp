a3438a2f5a5c868c419551fc3d8a6288f9c42e27
MeshCollider <dobsonsa68@gmail.com>
2017-12-06 00:24:06 +1300

    Test datadir in conf file exists

--- src/util.cpp
+++ src/util.cpp
@@ -528,6 +528,9 @@ void ArgsManager::ReadConfigFile(const std::string& confPath)
     }
     // If datadir is changed in .conf file:
     ClearDatadirCache();
+    if (!fs::is_directory(GetDataDir(false))) {
+        throw std::runtime_error(strprintf("specified data directory \"%s\" does not exist.", gArgs.GetArg("-datadir", "").c_str()));
+    }
 }
 
 fs::path AbsPathForConfigVal(const fs::path& path, bool net_specific)
