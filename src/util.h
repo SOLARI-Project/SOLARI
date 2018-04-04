1bddffe569bf0f8dbeb93f5c665abf68df720a14
Anthony Towns <aj@erisian.com.au>
2018-04-04 18:06:00 +1000

    ArgsManager: limit some options to only apply on mainnet when in default section
    When specified in bitcoin.conf without using the [regtest] or [test]
    section header, or a "regtest." or "test." prefix, the "addnode",
    "connect", "port", "bind", "rpcport", "rpcbind", and "wallet" settings
    will only be applied when running on mainnet.

--- src/util.h
+++ src/util.h
@@ -27,6 +27,8 @@
 #include <atomic>
 #include <exception>
 #include <map>
+#include <memory>
+#include <set>
 #include <stdint.h>
 #include <string>
 #include <unordered_set>
@@ -126,10 +128,13 @@ protected:
     std::map<std::string, std::vector<std::string>> m_override_args;
     std::map<std::string, std::vector<std::string>> m_config_args;
     std::string m_network;
+    std::set<std::string> m_network_only_args;
 
     void ReadConfigStream(std::istream& stream);
 
 public:
+    ArgsManager();
+
     /**
      * Select the network in use
      */
