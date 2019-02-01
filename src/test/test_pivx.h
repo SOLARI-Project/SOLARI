c82e359135f0788e3a74df8ed603e89d0f853dbf
MarcoFalke <falke.marco@gmail.com>
2019-02-01 14:06:32 -0800

    test: Make bloom tests deterministic

--- src/test/test_pivx.h
+++ src/test/test_pivx.h
@@ -13,6 +13,11 @@
 
 extern FastRandomContext insecure_rand_ctx;
 
+/**
+ * Flag to make GetRand in random.h return the same number
+ */
+extern bool g_mock_deterministic_tests;
+
 static inline void SeedInsecureRand(bool deterministic = false)
 {
     insecure_rand_ctx = FastRandomContext(deterministic);
