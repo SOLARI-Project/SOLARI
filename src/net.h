3b832c9349fb495f01041ca76041e0a419de6c0d
Amiti Uttarwar <amiti@uttarwar.org>
2019-11-05 11:06:53 +0100

    [tools] add PoissonNextSend method that returns mockable time

--- src/net.h
+++ src/net.h
@@ -810,9 +810,13 @@ public:
     static void callCleanup();
 };
 
-
-
 /** Return a timestamp in the future (in microseconds) for exponentially distributed events. */
 int64_t PoissonNextSend(int64_t nNow, int average_interval_seconds);
+
+/** Wrapper to return mockable type */
+inline std::chrono::microseconds PoissonNextSend(std::chrono::microseconds now, std::chrono::seconds average_interval)
+{
+    return std::chrono::microseconds{PoissonNextSend(now.count(), average_interval.count())};
+}
 
 #endif // BITCOIN_NET_H
