0c064f8fab7424cfb7736485884d4799b0a40fca
random-zebra <random.zebra@protonmail.com>
2021-01-26 01:53:09 +0100

    [Tests] Add tests for SetTxPayload/GetTxPayload and CheckStringSig

---
+++ src/test/evo_specialtx_tests.cpp
@@ -0,0 +1,92 @@
+// Copyright (c) 2021 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://www.opensource.org/licenses/mit-license.php.
+
+#include "test/test_pivx.h"
+#include "primitives/transaction.h"
+#include "evo/providertx.h"
+#include "evo/specialtx.h"
+#include "messagesigner.h"
+#include "netbase.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(evo_specialtx_tests, TestingSetup)
+
+static void RandomScript(CScript &script)
+{
+    static const opcodetype oplist[] = {OP_FALSE, OP_1, OP_2, OP_3, OP_CHECKSIG, OP_IF, OP_VERIF, OP_RETURN, OP_CODESEPARATOR};
+    script = CScript();
+    int ops = (InsecureRandRange(10));
+    for (int i=0; i<ops; i++)
+        script << oplist[InsecureRandRange(sizeof(oplist)/sizeof(oplist[0]))];
+}
+
+static CKey GetRandomKey()
+{
+    CKey key;
+    key.MakeNewKey(true);
+    return key;
+}
+
+static CKeyID GetRandomKeyID()
+{
+    return GetRandomKey().GetPubKey().GetID();
+}
+
+static ProRegPL GetRandomProRegPayload()
+{
+    ProRegPL pl;
+    pl.collateralOutpoint.hash = GetRandHash();
+    pl.collateralOutpoint.n = InsecureRandBits(2);
+    BOOST_CHECK(Lookup("127.0.0.1:51472", pl.addr, Params().GetDefaultPort(), false));
+    pl.keyIDOwner = GetRandomKeyID();
+    pl.keyIDOperator = GetRandomKeyID();
+    pl.keyIDVoting = GetRandomKeyID();
+    RandomScript(pl.scriptPayout);
+    pl.nOperatorReward = InsecureRandRange(10000);
+    RandomScript(pl.scriptOperatorPayout);
+    pl.inputsHash = GetRandHash();
+    pl.vchSig = InsecureRandBytes(63);
+    return pl;
+}
+
+BOOST_AUTO_TEST_CASE(providertx_setpayload_test)
+{
+    const ProRegPL& pl = GetRandomProRegPayload();
+
+    CMutableTransaction mtx;
+    SetTxPayload(mtx, pl);
+    ProRegPL pl2;
+    BOOST_CHECK(GetTxPayload(mtx, pl2));
+    BOOST_CHECK(pl.collateralOutpoint == pl2.collateralOutpoint);
+    BOOST_CHECK(pl.addr  == pl2.addr);
+    BOOST_CHECK(pl.keyIDOwner == pl2.keyIDOwner);
+    BOOST_CHECK(pl.keyIDOperator == pl2.keyIDOperator);
+    BOOST_CHECK(pl.keyIDVoting == pl2.keyIDVoting);
+    BOOST_CHECK(pl.scriptPayout == pl2.scriptPayout);
+    BOOST_CHECK(pl.nOperatorReward  == pl2.nOperatorReward);
+    BOOST_CHECK(pl.scriptOperatorPayout == pl2.scriptOperatorPayout);
+    BOOST_CHECK(pl.inputsHash == pl2.inputsHash);
+    BOOST_CHECK(pl.vchSig == pl2.vchSig);
+}
+
+BOOST_AUTO_TEST_CASE(providertx_checkstringsig_test)
+{
+    ProRegPL pl = GetRandomProRegPayload();
+    pl.vchSig.clear();
+    const CKey& key = GetRandomKey();
+    BOOST_CHECK(CMessageSigner::SignMessage(pl.MakeSignString(), pl.vchSig, key));
+
+    std::string strError;
+    const CKeyID& keyID = key.GetPubKey().GetID();
+    BOOST_CHECK(CMessageSigner::VerifyMessage(keyID, pl.vchSig, pl.MakeSignString(), strError));
+    // Change owner address or script payout
+    pl.keyIDOwner = GetRandomKeyID();
+    BOOST_CHECK(!CMessageSigner::VerifyMessage(keyID, pl.vchSig, pl.MakeSignString(), strError));
+    RandomScript(pl.scriptPayout);
+    BOOST_CHECK(!CMessageSigner::VerifyMessage(keyID, pl.vchSig, pl.MakeSignString(), strError));
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
