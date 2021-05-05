8ff6fa2050ef33f6290befe887202d42fdbc91dc
furszy <matiasfurszyfer@protonmail.com>
2021-05-05 12:03:16 -0300

    [test] validation_block_tests: New ASSERT_WITH_MSG to print dynamic error messages.
     BOOST_ASSERT_MSG only prints static string messages and that isn't enough to understand possible problems.

--- src/test/validation_block_tests.cpp
+++ src/test/validation_block_tests.cpp
@@ -14,6 +14,8 @@
 #include "validation.h"
 #include "validationinterface.h"
 
+#define ASSERT_WITH_MSG(cond, msg) if (!cond) { BOOST_ERROR(msg); }
+
 struct RegtestingSetup : public TestingSetup {
     RegtestingSetup() : TestingSetup(CBaseChainParams::REGTEST) {}
 };
@@ -157,7 +159,7 @@ BOOST_AUTO_TEST_CASE(processnewblock_signals_ordering)
                     if (state.GetRejectReason() == "duplicate" ||
                         state.GetRejectReason() == "prevblk-not-found" ||
                         state.GetRejectReason() == "bad-prevblk") continue;
-                    BOOST_ASSERT_MSG(processed,  ("Error: " + state.GetRejectReason()).c_str());
+                    ASSERT_WITH_MSG(!processed,  ("Error: " + state.GetRejectReason()).c_str());
                 }
             }
         });
