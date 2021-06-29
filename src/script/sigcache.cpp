79b1e50252509d42902f49a68335681bb024a287
random-zebra <random.zebra@protonmail.com>
2021-06-29 12:56:41 +0200

    Refactor: implement CPubKey::data()

--- src/script/sigcache.cpp
+++ src/script/sigcache.cpp
@@ -39,7 +39,7 @@ public:
     void
     ComputeEntry(uint256& entry, const uint256 &hash, const std::vector<unsigned char>& vchSig, const CPubKey& pubkey)
     {
-        CSHA256().Write(nonce.begin(), 32).Write(hash.begin(), 32).Write(&pubkey[0], pubkey.size()).Write(vchSig.data(), vchSig.size()).Finalize(entry.begin());
+        CSHA256().Write(nonce.begin(), 32).Write(hash.begin(), 32).Write(pubkey.data(), pubkey.size()).Write(vchSig.data(), vchSig.size()).Finalize(entry.begin());
     }
 
     bool
