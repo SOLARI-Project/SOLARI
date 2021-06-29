020ac1684541c69960ed34d82b4b9bfb57c409a4
random-zebra <random.zebra@protonmail.com>
2021-06-29 16:39:31 +0200

    Init: Remove redundant exit(EXIT_FAILURE) instances and replace with
    return false
    
    >>> backports bitcoin@b296bf1496b213eaea53cac0a734679fed7a42f5

--- src/pivxd.cpp
+++ src/pivxd.cpp
@@ -105,7 +105,7 @@ bool AppInit(int argc, char* argv[])
         for (int i = 1; i < argc; i++) {
             if (!IsSwitchChar(argv[i][0])) {
                 fprintf(stderr, "Error: Command line contains unexpected token '%s', see pivxd -h for a list of options.\n", argv[i]);
-                exit(EXIT_FAILURE);
+                return false;
             }
         }
 
@@ -116,15 +116,15 @@ bool AppInit(int argc, char* argv[])
         InitParameterInteraction();
         if (!AppInitBasicSetup()) {
             // UIError will have been called with detailed error, which ends up on console
-            exit(1);
+            return false;
         }
         if (!AppInitParameterInteraction()) {
             // UIError will have been called with detailed error, which ends up on console
-            exit(1);
+            return false;
         }
         if (!AppInitSanityChecks()) {
             // UIError will have been called with detailed error, which ends up on console
-            exit(1);
+            return false;
         }
 
 #ifndef WIN32
