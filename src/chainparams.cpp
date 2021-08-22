5168b05daa62f16201d064c323a48f9af8b815b2
furszy <matiasfurszyfer@protonmail.com>
2021-08-22 11:40:47 -0300

    Add mainnet DNS seeder from furszy.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -232,6 +232,7 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         vSeeds.emplace_back("pivx.seed.fuzzbawls.pw", true);     // Primary DNS Seeder from Fuzzbawls
         vSeeds.emplace_back("pivx.seed2.fuzzbawls.pw", true);    // Secondary DNS Seeder from Fuzzbawls
+        vSeeds.emplace_back("pivx-seed.furszy.net", true);     // Primary DNS Seeder from furszy
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 30);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 13);
