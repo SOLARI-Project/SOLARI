b3chain/b3chain

Improved microbenchmarking with multiple features.
* inline performance critical code
* Average runtime is specified and used to calculate iterations.
* Console: show median of multiple runs
* plot: show box plot
* filter benchmarks
* specify scaling factor
* ignore src/test and src/bench in command line check script
* number of iterations instead of time
* Replaced runtime in BENCHMARK makro number of iterations.
* Added -? to bench_bitcoin
* Benchmark plotly.js URL, width, height can be customized
* Fixed incorrect precision warning

src/bench/lockedpool.cpp
@@ -43,5 +43,4 @@ static void BenchLockedPool(benchmark::State& state)
     addr.clear();
 }
 
-BENCHMARK(BenchLockedPool);
-
+BENCHMARK(BenchLockedPool, 530);

SOLARI-Project/SOLARI

src/bench/lockedpool.cpp
@@ -43,5 +43,4 @@ static void LockedPool(benchmark::State& state)
     addr.clear();
 }
 
-BENCHMARK(LockedPool);
-
+BENCHMARK(LockedPool, 530);