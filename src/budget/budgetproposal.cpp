fa2ab1073103e08bd757f6f5d255e55638bbb536
random-zebra <random.zebra@protonmail.com>
2021-12-07 12:11:38 +0100

    [Refactor] Budget: check mn-enabled/vote-time before adding to mapSeen

--- src/budget/budgetproposal.cpp
+++ src/budget/budgetproposal.cpp
@@ -239,12 +239,6 @@ bool CBudgetProposal::AddOrUpdateVote(const CBudgetVote& vote, std::string& strE
         strAction = "Existing vote updated:";
     }
 
-    if (voteTime > GetTime() + (60 * 60)) {
-        strError = strprintf("new vote is too far ahead of current time - %s - nTime %lli - Max Time %lli\n", vote.GetHash().ToString(), voteTime, GetTime() + (60 * 60));
-        LogPrint(BCLog::MNBUDGET, "%s: %s\n", __func__, strError);
-        return false;
-    }
-
     mapVotes[mnId] = vote;
     LogPrint(BCLog::MNBUDGET, "%s: %s %s\n", __func__, strAction.c_str(), vote.GetHash().ToString().c_str());
 
