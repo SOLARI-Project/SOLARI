6358d7016ee206e4a3241db86dc17ed1927295b2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-16 21:34:08 -0300

    Fix datadir handling in pivx-cli
    This prevents premature tries to access or create the default datadir.
    This is useful when the -datadir option is specified and the default
    datadir is unreachable.

--- src/pivx-cli.cpp
+++ src/pivx-cli.cpp
@@ -101,7 +101,7 @@ static bool AppInitRPC(int argc, char* argv[])
         fprintf(stdout, "%s", strUsage.c_str());
         return false;
     }
-    if (!fs::is_directory(GetDataDir(false))) {
+    if (!CheckDataDirOption()) {
         fprintf(stderr, "Error: Specified data directory \"%s\" does not exist.\n", gArgs.GetArg("-datadir", "").c_str());
         return false;
     }
