furszy/bitcoin-core

P2P: Ensure intra-quorum connections and only relay to members

test/lint/lint-circular-dependencies.sh
@@ -57,8 +57,8 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "chain -> legacy/stakemodifier -> validation -> checkpoints -> chain"
     "chain -> legacy/stakemodifier -> validation -> undo -> chain"
     "chain -> legacy/stakemodifier -> validation -> pow -> chain"
+    "evo/deterministicmns -> masternode -> net -> tiertwo/net_masternodes -> evo/deterministicmns"
     "evo/deterministicmns -> masternodeman -> validation -> validationinterface -> evo/deterministicmns"
-    "evo/deterministicmns -> llmq/quorums_utils -> net -> tiertwo/net_masternodes -> evo/deterministicmns"
 )
 
 EXIT_CODE=0

SOLARI-Project/SOLARI

test/lint/lint-circular-dependencies.sh
@@ -48,8 +48,8 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "chain -> legacy/stakemodifier -> validation -> checkpoints -> chain"
     "chain -> legacy/stakemodifier -> validation -> undo -> chain"
     "chain -> legacy/stakemodifier -> validation -> pow -> chain"
+    "evo/deterministicmns -> masternodeman -> net -> tiertwo/net_masternodes -> evo/deterministicmns"
     "evo/deterministicmns -> masternodeman -> validation -> validationinterface -> evo/deterministicmns"
-    "evo/deterministicmns -> llmq/quorums_utils -> net -> tiertwo/net_masternodes -> evo/deterministicmns"
 )
 
 EXIT_CODE=0