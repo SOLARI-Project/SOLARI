eaae5f93bf76b4c991cdf37da7963f3051908286
tohsnoom <moonshot@phore.io>
2022-08-03 19:48:05 -0500

    [QA] Added v5.5 budget unit tests

--- src/test/budget_tests.cpp
+++ src/test/budget_tests.cpp
@@ -52,6 +52,17 @@ BOOST_AUTO_TEST_CASE(budget_value)
     SelectParams(CBaseChainParams::MAIN);
     nHeightTest = Params().GetConsensus().vUpgrades[Consensus::UPGRADE_ZC_V2].nActivationHeight + 1;
     CheckBudgetValue(nHeightTest, "mainnet", 43200*COIN);
+
+    SelectParams(CBaseChainParams::TESTNET);
+    nHeightTest = Params().GetConsensus().vUpgrades[Consensus::UPGRADE_V5_5].nActivationHeight + 1;
+    CheckBudgetValue(nHeightTest-1, "testnet", 144*COIN);
+    CheckBudgetValue(nHeightTest, "testnet", 1440*COIN);
+
+    SelectParams(CBaseChainParams::MAIN);
+    nHeightTest = Params().GetConsensus().vUpgrades[Consensus::UPGRADE_V5_5].nActivationHeight + 1;
+    CheckBudgetValue(nHeightTest-1, "mainnet", 43200*COIN);
+    CheckBudgetValue(nHeightTest, "mainnet", 432000*COIN);
+
 }
 
 BOOST_FIXTURE_TEST_CASE(block_value, TestnetSetup)
